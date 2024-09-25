90bfc79260e2ff78951031eb07becd81e5d95244
Juliana Zacharias <julianazacharias.dev@gmail.com>
2024-09-24 22:53:14 -0300

    🌟 feat: basic user crud with auth and tests

--- digital_library/app.py
+++ digital_library/app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from digital_library.routers import auth, users
+
 app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get('/')
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from digital_library.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
