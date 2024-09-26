37a84f32d5367c8c7b341b8a37be8a9a003faf28
Juliana Zacharias <julianazacharias.dev@gmail.com>
2024-09-25 22:44:33 -0300

    🌟 feat: improving authentication with refresh token and tests with factory

--- digital_library/routers/auth.py
+++ digital_library/routers/auth.py
@@ -9,7 +9,11 @@ from sqlalchemy.orm import Session
 from digital_library.database import get_session
 from digital_library.models import User
 from digital_library.schemas import Token
-from digital_library.security import create_access_token, verify_password
+from digital_library.security import (
+    create_access_token,
+    get_current_user,
+    verify_password,
+)
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -36,3 +40,12 @@ def login_for_access_token(form_data: OAuth2Form, session: Session):
     access_token = create_access_token(data={'sub': user.email})
 
     return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+@router.post('/refresh_token', response_model=Token)
+def refresh_access_token(
+    user: User = Depends(get_current_user),
+):
+    new_access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': new_access_token, 'token_type': 'bearer'}
